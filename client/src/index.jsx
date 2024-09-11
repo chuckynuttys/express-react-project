40d4ca95644b47f749bf5ef67bb72fdf819eca8f
chuckynuttys <charles25strain@gmail.com>
2024-09-10 22:07:13 -0500

    update

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -18,15 +18,4 @@ createRoot(document.getElementById('root')).render(
 
   </StrictMode>,
 )
-export default class App extends Component {
-  componentDidMount() {
-    console.log('I was triggered during componentDidMount')
-  }
-
-  render() {
-    console.log('I was triggered during render')
-    return ( 
-      <div> I am the App component </div>
-    )
-  }
-}
+//test
